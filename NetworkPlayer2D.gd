d19353d054ae6565c8ae2d9193db772660ca342b
ThaddeusWilliams <34045447+ThaddeusWilliams@users.noreply.github.com>
2025-09-22 21:34:39 -0400

    Update NetworkPlayer2D.gd

--- NetworkPlayer2D.gd
+++ NetworkPlayer2D.gd
@@ -4,25 +4,22 @@ extends CharacterBody2D
 var is_player = false ## Always means that this is the player being controlled
 var peer_id = 1 ## Unique id of Network.multiplayer_peer
 
-func _ready():
-	#name = str(get_multiplayer_authority())
-	pass
 func _process(delta):
-	is_player = peer_id == Network.peer_get_id() or !Network.is_online
-	$Label.text = str(peer_id)
-
+	is_player = (peer_id == Network.peer_get_id()) or !Network.is_online
 
 func _physics_process(delta):
+	%Label.text = "npeer id:"+str(Network.peer_get_id())+"\npeer id:" + str(peer_id) + "\ncount: "+str(Network.players.size()) +"\n"+ str(Network.multiplayer.get_peers())
+	if !Network.is_online:
+		if Input.is_action_just_pressed("HOST"):
+			Network.host()
+			Network.peer_id_assign(self, Network.peer_get_id())
+		if Input.is_action_just_pressed("JOIN"):
+			Network.join()
+			Network.peer_id_assign(self, Network.peer_get_id())
 	if is_player:
-		process_controls(delta)
-		process_animations()
-		## Network portion of code
-		$Camera2D.make_current()
-		look_position = get_global_mouse_position()
-		velocity = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown") * speed * delta
+		velocity += Input.get_vector("ui_left","ui_right","ui_up","ui_down") * 50
+		velocity /= 1.1
+		Network.variable_set.rpc("position" + str(peer_id), global_position)
 		move_and_slide()
-		Network.variable_set(str(peer_id) + ":position", position)
 	else:
-		position = Network.variable_get(str(peer_id) + ":position", Vector2())
-		
-
+		global_position = Network.variable_get("position" + str(peer_id), global_position)
