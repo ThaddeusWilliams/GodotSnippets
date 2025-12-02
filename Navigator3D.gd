8c2bfc8d40f2248572a2c88671166914d42e7492
ThaddeusWilliams <34045447+ThaddeusWilliams@users.noreply.github.com>
2025-12-01 20:17:30 -0500

    Initialize navigation agent with add_node_parented
    Updated navigation agent initialization and added a helper function to add nodes.

--- Navigator3D.gd
+++ Navigator3D.gd
@@ -1,12 +1,13 @@
 extends CharacterBody3D
 
 @export var ai_movement_speed: float = 4.0
-@onready var navigation_agent: NavigationAgent3D = $Agent
+@onready var navigation_agent : NavigationAgent3D = add_node_parented(NavigationAgent3D.new())
 
 var ai_destination : Vector3
 
 func _ready() -> void:
 	move_to(ai_destination)
+	navigation_agent.avoidance_enabled = true
 
 func move_to(movement_target: Vector3):
 	navigation_agent.set_target_position(movement_target)
@@ -16,3 +17,7 @@ func _physics_process(delta):
 	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * ai_movement_speed * delta
 	velocity = new_velocity
 	move_and_slide()
+
+func add_node_parented(n: Node):
+	add_child(n)
+	return n
